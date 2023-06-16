be25afe8b7e8ebd1d860083ac8a50bd6b8b8ea75
Eliza Fury <eliza.fury@onemodel.co>
2023-06-16 12:20:04 +1000

    Exercise1: add to website

--- ControlFlow/Exercise 1/Excercise.js
+++
@@ -1,2 +0,0 @@
-/* Write a a fucntion that takes two numbers and returns the maximum of the tw */
-
---
+++ ControlFlow/Exercise 1/Exercise.js
@@ -0,0 +1,11 @@
+/* Write a a fucntion that takes two numbers and returns the maximum of the two */
+
+function solve() {
+    var firstNumber = document.getElementById("firstNumber").value;
+    var secondNumber = document.getElementById("secondNumber").value;
+
+    if ( firstNumber < secondNumber ) {
+        console.log("firstNumber is less than second number");
+    }
+    
+}
