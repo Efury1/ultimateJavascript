392ab0b4aa5bdfa571f18a6d76e4cd437143d4c7
Eliza Fury <eliza.fury@onemodel.co>
2023-06-19 11:07:41 +1000

    ControlFlow: Update FizzBuzz exercise

--- ControlFlow/Exercises/Exercise3.js
+++ ControlFlow/Exercises/Exercise3.js
@@ -2,22 +2,17 @@
 console.log(output);
 
 function fizzBuzz(input) {
-    if ((output % 3) && (output % 5)) {
-        console.log('fizzBuz')
-    }
-    else if ((output % 3) == 0) {
-        console.log('Fizz')
-    } 
-    else if ((input % 5) == 0) {
-        console.log('Buz')
-    }
-    else if ((isNaN) == true) {
-        console.log('Not a Number')
-    }
-    else {
-        console.log()
+    if (typeof input !== 'number') 
+        return NaN ;
 
-    }
+    if ((output % 3) && (output % 5)) 
+        return 'FizzBuzz';
+
+   if (output % 3 == 0) 
+        return 'Fizz';
     
- 
+    if ( input % 5 == 0) 
+        return 'Buzz';
+    
+    return input;
 }
