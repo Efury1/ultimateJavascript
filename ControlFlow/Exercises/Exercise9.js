3c0730a668d07bf995420cca569b2a01fd1540f4
Eliza Fury <eliza.fury@onemodel.co>
2023-06-21 12:17:12 +1000

    ControlFlow: Exercise 10: show Prime numbers

--- ControlFlow/Exercises/Exercise9.js
+++ ControlFlow/Exercises/Exercise9.js
@@ -5,11 +5,11 @@ function showStars(rows) {
         let pattern = '';
         for (let row = 1; row <= rows; row++) {
             let pattern = '';
-            for (let i = 0; i < row; i++) {
-                pattern += '*''
-            }
         }
-
+        for (let i = 0; i < row; i++) {
+                pattern += '*';
+        }
+            console.log(pattern);
     }
 
 }
