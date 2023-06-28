5cab7251b30ba72fd11620c9c2d998d3de551c27
Eliza Fury <eliza.fury@onemodel.co>
2023-06-28 12:03:16 +1000

    Exercise 20: currect syntax for priceRange

---
+++ Objects/Exercise20.js
@@ -0,0 +1,9 @@
+let priceRange = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
+]
+
+let restraunts = [
+    {averagePerPerson: 5}
+]
