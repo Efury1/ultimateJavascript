fec5bea44b299544df98301e1cb1e8ef9c8ef94d
Eliza Fury <eliza.fury@onemodel.co>
2023-06-29 12:07:36 +1000

    Finish exercise 19

--- Objects/Exercise19.js
+++ Objects/Exercise19.js
@@ -1,13 +1,18 @@
-let post = {
-    title: 'a',
-    body: 'b',
-    author: 'c',
-    views: 10,
-    comments: [
-        {author: 'a', body: 'b'},
-        {author: 'c', body: 'd'},
-    ],
-    isLive: true
-};
+//create constructor function
+/* 
+* For a new post that haven't been published yet, we don't want to add a
+* parameter in the function
+*/
+
+let post = new Post('a', 'b', 'c');
 
 console.log(post);
+
+function Post(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
