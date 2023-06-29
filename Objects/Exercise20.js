9ba3a9b66909ed482e65638dbc9c10824d33ba58
Eliza Fury <eliza.fury@onemodel.co>
2023-06-29 14:46:54 +1000

    Complete Objects selection and start on Arrays folder

--- Objects/Exercise20.js
+++ Objects/Exercise20.js
@@ -1,3 +1,5 @@
+// label for user, tooltip, the average prices for filtering
+
 let priceRange = [
     { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
     { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
