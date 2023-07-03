6edc954cf867de6028548035a2eff096bc691048
Eliza Fury <eliza.fury@onemodel.co>
2023-07-03 13:35:49 +1000

    Arrays: Complete exercise 16

--- Arrays/Exercise15.js
+++ Arrays/Exercise15.js
@@ -1,12 +1,16 @@
 const numbers = [1, -1, 2, 3];
 
-const filtered = numbers.filter(n => n >= 0).map(n => ({ value: n}));
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value: n}))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
 //const items = filtered.map(n => '<li>' +  n + '</li>')
 //const html = '<ul>' + items.join('') + '</ul>';
 
 //can map to object
 //curly brackets represent a code block
-const items = filtered.map(n => ({ value: n}));
+//const items = filtered.map(n => ({ value: n}));
 
 //displace items const
 console.log(items);
