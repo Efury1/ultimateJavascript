03770ebe4a516f392b9e6e4efb7fa670a1ef2b4e
Eliza Fury <eliza.fury@onemodel.co>
2023-07-06 17:56:43 +1000

    Functions: complete execise 5

--- Functions/Exercise4.js
+++ Functions/Exercise4.js
@@ -3,4 +3,4 @@ function sum(discount, ...prices) {
     const total = prices.reduce((a,b) => a + b);
     return total * (1 - discount);
 }
-console.log(sum(0.1, 20, 30, 1));
+console.log(sum(0.1, 20, 30));
