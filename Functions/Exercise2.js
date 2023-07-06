ae1093a180c0761d96c9d52f001f95aabf88ce61
Eliza Fury <eliza.fury@onemodel.co>
2023-07-06 16:33:05 +1000

    Functions: work on exercise 1 and 2

---
+++ Functions/Exercise2.js
@@ -0,0 +1,13 @@
+//Function Declaration
+function walk() {
+    console.log('walk');
+}
+
+// Anoymous Function Expression
+let run = function() {
+    console.log('run');
+};
+
+let move = run;
+run();
+move();
