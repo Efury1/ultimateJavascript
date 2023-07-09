1af872c4bc6156cb2f13496c777566ef7494d37e
Eliza Fury <eliza.fury@onemodel.co>
2023-07-09 23:38:32 +1000

    Function: exercise 10

---
+++ Functions/Exercise10.js
@@ -0,0 +1,25 @@
+/*const video = {
+    title: 'a',
+    play() {
+        console.log(this);
+    }
+};
+
+function Video(title) {
+    this.title = title;
+    console.log(this);
+}
+
+const v = new Video('b');*/
+
+const video = {
+    title: 'a',
+    tags: ['a', 'b', 'c'],
+    showTags() {
+        this.tags.forEach(function(tag) {
+            console.log(this.title, tag);
+        }, this);
+    }
+};
+
+video.showTags();
